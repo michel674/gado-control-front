e2863431705ba73da6b0831b5397f2ce18eba382
Michel Elesbao de Jesus <michel.elesbao@usp.br>
2021-12-11 15:17:33 -0300

    first commit

---
+++ src/components/button.styled.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: green;
+  color: white;
+
+  padding: 5px 10px;
+
+  border: none;
+  border-radius: 3px;
+
+  cursor: pointer;
+;
+
+`
