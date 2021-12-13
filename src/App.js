3c27d6433a1b9dd0b9300c64e2a597d58ecbdbec
Michel Elesbao de Jesus <michel.elesbao@usp.br>
2021-12-13 16:49:48 -0300

    remove console log

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import { AppRoutes } from './app-routes';
 import Header from './Header';
 import Navbar from './Navbar';
 import { Separator } from './atomic/atm.separator/separator.styled';
-import { useLocation } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 
 import { GlobalStyled } from './components/reset.styled';
 
 const App = () => {
   const pathname = window.location.pathname;
 
-  console.log(pathname);
   return (
     <BrowserRouter>
       <GlobalStyled />
