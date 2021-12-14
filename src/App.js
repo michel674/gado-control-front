5ce7a94d811b1088b014c9483f30bcdcd0e0081a
Michel Elesbao de Jesus <michel.elesbao@usp.br>
2021-12-13 22:52:41 -0300

    fix app

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
     <BrowserRouter>
       <GlobalStyled />
       {pathname !== '/login' && <Header />}
-      <AppRoutes />
       <AppWrapperStyled>
         <AppRoutes />
       </AppWrapperStyled>
